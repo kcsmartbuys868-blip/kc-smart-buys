d5a740c7ec6b737fac59b91bc24436f5fd1daca6
KC Smart Buys <kcsmartbuys868@gmail.com>
2026-08-16 22:47:53 -0400

    Fix shop page production build

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,8 +1,13 @@
+import { Suspense } from "react";
 import { getProducts } from "@/lib/api";
 import ShopContent from "./shopContent";
 
 export default async function ShopPage() {
   const products = await getProducts();
 
-  return <ShopContent products={products} />;
+  return (
+    <Suspense fallback={null}>
+      <ShopContent products={products} />
+    </Suspense>
+  );
 }
